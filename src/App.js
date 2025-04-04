1df50697f6ac1163c94f1e600bcadecf7d6e3b7d
Bhuvana0210 <sinchanaodiyoor@gmail.com>
2025-04-04 20:20:50 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+// // import './App.css';
+// import React from 'react';
+// import SplashScreen from './Components/splashScreen';
 
-function App() {
-  return (
+
+// function App() {
+//   return (
+//     <div className="App">
+//        <SplashScreen/>
+       
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+import React from "react";
+// import SplashScreen from "./Components/splashScreen";
+import Nav from "./Components/navBar";
+
+function App(){
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <SplashScreen/> */}
+      <Nav/>
     </div>
-  );
+  )
 }
 
 export default App;
